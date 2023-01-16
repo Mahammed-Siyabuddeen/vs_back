28ab2c76bb1511ae660a74cdfe154cd125798d42
Mahammed-Siyabuddeen <mohd.sihab615@gmail.com>
2023-01-16 17:42:06 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from "express";
-
+import cors from 'cors'
+import bodyParser from "body-parser";
+import env from  'dotenv'
+import mongoose from "mongoose";
 const app=express()
+env.config()
 const port=process.env.PORT||9000
 app.listen(port,()=>console.log(`listening port ${port}`))
 
+app.use(bodyParser.json({limit:'50mb',extended:true}))
+app.use(bodyParser.urlencoded({limit:'50mb',extended:true}))
+app.use(cors)
 app.use('/',(req,res)=>{
     res.send("hello wolrd")
+})
+
+mongoose.connect(process.env.CONNECTIONURL,{
+    useNewUrlParser:true
 })
